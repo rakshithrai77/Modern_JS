4f42671bbe55ea685f903c8c50e32f9609f520bb
“rakshithrai77” <rakshithrai77@gmail.com>
2023-02-27 14:22:40 +0530

    commit

--- Modules/script2.js
+++ Modules/script2.js
@@ -1,2 +1,10 @@
-import {admin} from './script3.js';
-admin.a=66;
+import * as g from './script3.js';
+//import User from './user.js'; -->defualt export
+//import {admin as a,sayHi as hi} from './script3.js';;
+g.admin.a=66;
+console.log(g.admin.a)
+g.sayHi()
+//A module has either named exports or the default one.
+/*export class { // Error! (non-default export needs a name)
+    constructor() {}
+  }*/
--- Modules/script3.js
+++ Modules/script3.js
@@ -1,2 +1,34 @@
-export let admin={ a:67
+//export before decalartions
+ let admin={ a:67
 }
+
+function sayHi()
+{
+    console.log('hi')
+}
+console.log(admin.a)
+export {sayHi,admin}
+//export {sayHi as a,admin as b}
+//export default
+export default class User { 
+    constructor(name) {
+      this.name = name;
+    }
+  }
+//
+// same as if we added "export default" before the function
+export {sayHi as default};//default can be exported 
+//default import we can import any name
+//Rexport
+export {default as User} from './user.js';//rexport default
+
+export {login, logout} from './helpers.js';
+
+// re-export the default export as User
+export {default as User} from './user.js';
+//is that re-exported modules aren’t available in the current file.
+
+export {login, logout} from './helpers.js';
+
+export * from './user.js'; // to re-export named exports
+export {default} from './user.js'; // to re-export the default export
